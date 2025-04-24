3dc5e6ac66ba3e126197d0550ee0bac797500b7f
sophchew <98368063+sophchew@users.noreply.github.com>
2025-04-23 20:35:40 -0400

    Initial commit. ClaudeAPI.java contains a singular method that makes a call to the AI with the specified input, using the ClaudeAPI Java library.

---
+++ src/main/java/ClaudeAPI.java
@@ -0,0 +1,25 @@
+import com.anthropic.client.AnthropicClient;
+import com.anthropic.client.okhttp.AnthropicOkHttpClient;
+import com.anthropic.models.messages.Message;
+import com.anthropic.models.messages.MessageCreateParams;
+import com.anthropic.models.messages.Model;
+
+public class ClaudeAPI {
+
+    public static void chat(String query) {
+        AnthropicClient client = AnthropicOkHttpClient.builder()
+                .apiKey("") // removed API key
+                .build();
+
+        MessageCreateParams createParams = MessageCreateParams.builder()
+                .model(Model.CLAUDE_3_5_SONNET_LATEST)
+                .maxTokens(2048)
+                .addUserMessage(query)
+                .build();
+
+        client.messages().create(createParams).content().stream()
+                .flatMap(contentBlock -> contentBlock.text().stream())
+                .forEach(textBlock -> System.out.println(textBlock.text()));
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public final class Main {
+
+    public static void main(String[] args) {
+        ClaudeAPI.chat("Hi claude i am testing my java code for claude api and need a sign this works");
+    }
+}
